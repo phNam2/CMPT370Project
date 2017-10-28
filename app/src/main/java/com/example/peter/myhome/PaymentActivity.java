aac8f3bad984fa4b7fffe92472e04d6833660ce8
Nam Pham <hnp435@mail.usask.ca>
2017-10-28 15:35:12 -0600

    Bring in payment java class

---
+++ app/src/main/java/com/example/peter/myhome/PaymentActivity.java
@@ -0,0 +1,19 @@
+package com.example.peter.myhome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nam on 2017-10-28.
+ */
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_first_page);
+    }
+
+    
+}
