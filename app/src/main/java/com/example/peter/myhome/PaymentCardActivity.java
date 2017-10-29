a1dd97b4f2425f5ca2ca677f89c60a3e7a937aab
Nam Pham <hnp435@mail.usask.ca>
2017-10-29 01:28:42 -0600

    adding payment confirm page and some of its features

--- app/src/main/java/com/example/peter/myhome/PaymentCardActivity.java
+++ app/src/main/java/com/example/peter/myhome/PaymentCardActivity.java
@@ -1,7 +1,10 @@
 package com.example.peter.myhome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Nam on 2017-10-28.
@@ -13,5 +16,15 @@ public class PaymentCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paying_card_page);
+    }
+
+    public void onButtonPayClick1(View v) {
+        Button pay = (Button) findViewById(R.id.pay_button);
+        pay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(PaymentCardActivity.this, PaymentConfirm.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/peter/myhome/PaymentPaypalActivity.java
+++ app/src/main/java/com/example/peter/myhome/PaymentPaypalActivity.java
@@ -1,7 +1,10 @@
 package com.example.peter.myhome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Nam on 2017-10-28.
@@ -13,5 +16,15 @@ public class PaymentPaypalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paying_paypal_page);
+    }
+
+    public void onButtonPayClick2(View v) {
+        Button pay = (Button) findViewById(R.id.login_button);
+        pay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(PaymentPaypalActivity.this, PaymentConfirm.class));
+            }
+        });
     }
 }
