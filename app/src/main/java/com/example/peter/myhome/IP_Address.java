363591ddf2cacb5d38eb285e530315e12ef6f756
RobotW <wutiantong1993@gmail.com>
2017-11-17 18:21:30 -0500

    Revert "search branch"
    This reverts commit 241316c9d362ff2647cc27bd01f5fda3a0a05523.

--- app/src/main/java/com/example/peter/myhome/IP_Address.java
+++ app/src/main/java/com/example/peter/myhome/IP_Address.java
@@ -18,7 +18,7 @@ public class IP_Address {
     //public static String adress = "192.168.0.10";
 
     //University secure wifi web address
-    public static String adress = "10.227.137.204";
+    public static String adress = "10.227.139.155";
 
     public static String getIPAdress () {
 
