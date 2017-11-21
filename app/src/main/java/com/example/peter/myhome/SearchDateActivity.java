4cac23357f3bc4c99fbeda50f0ba7ad9c1458796
RobotW <wutiantong1993@gmail.com>
2017-11-21 17:08:04 -0500

    fix page form and pages opening

---
+++ app/src/main/java/com/example/peter/myhome/SearchDateActivity.java
@@ -0,0 +1,45 @@
+package com.example.peter.myhome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toolbar;
+
+
+public class SearchDateActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_date);
+/*
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+ //       setSupportActionBar(toolbar);
+
+ //       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+ //               Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+ //                       .setAction("Action", null).show();
+            }
+        });
+*/
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+    public void SaveDate(View view) {
+
+    }
+
+    public void BackButton(View view) {
+        Intent intent = new Intent(SearchDateActivity.this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+}
