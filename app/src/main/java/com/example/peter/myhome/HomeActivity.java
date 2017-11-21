f47849e1bf9614b707ed75cfdd8a3594647d59bc
RobotW <wutiantong1993@gmail.com>
2017-11-20 22:21:27 -0500

    fix page opening

--- app/src/main/java/com/example/peter/myhome/HomeActivity.java
+++ app/src/main/java/com/example/peter/myhome/HomeActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.support.v7.widget.SearchView;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
