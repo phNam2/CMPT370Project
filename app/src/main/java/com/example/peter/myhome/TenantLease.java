7e087bd845f5cfe76f5cb99ecdcc7e3a25a9e1ff
Graeme <graemegibbings@gmail.com>
2017-11-20 20:43:14 -0600

    set ids so that java will interact with xml. Moved declarations inside the on create method. Set up button with onclick listener and set a toast to appear on clicking. Still working on checking all edittext fields in order to enable the button

--- app/src/main/java/com/example/peter/myhome/TenantLease.java
+++ app/src/main/java/com/example/peter/myhome/TenantLease.java
@@ -1,54 +1,19 @@
 package com.example.peter.myhome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class TenantLease extends AppCompatActivity {
 
-    //Text fields for tenant renewal form
-    EditText name = (EditText) findViewById(R.id.name);
-    EditText address = (EditText) findViewById(R.id.address);
-    EditText propertyName = (EditText) findViewById(R.id.propertyName);
-
-    //Radio Buttons for lease renewal
-    RadioButton yes = (RadioButton) findViewById(R.id.yes);
-    RadioButton no = (RadioButton) findViewByID(R.id.no);
-
-    public void onButtonSubmit(View v) {
-        Button submit = (Button) findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(TenantLease.this, TenantLeaseConfirm.class));
-            }
-        });
-    }
-
-    //still need method for sending submission to database once
-    //database connection is working
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_yes:
-                if (checked)
-                    // send info to database
-                    break;
-            case R.id.radio_no:
-                if (checked)
-                    // send info to database
-                    break;
-        }
-    }
 
 
     @Override
@@ -67,6 +32,70 @@ public class TenantLease extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //Text fields for tenant renewal form
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText address = (EditText) findViewById(R.id.address);
+        EditText propertyName = (EditText) findViewById(R.id.propertyName);
+
+        //Radio Buttons for lease renewal
+        RadioButton yes = (RadioButton) findViewById(R.id.yes);
+        RadioButton no = (RadioButton) findViewById(R.id.no);
+
+        //create button object for xml submit button
+        Button submit = (Button) findViewById(R.id.submit);
+        //set button to be unclickable after it's created. Will change after fields are filled out
+        submit.setClickable(false);
+        //when clicked the button sends a toast
+        
+        submit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast toast = new Toast(getApplicationContext());
+                toast.makeText(getApplicationContext(), "Thank You", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
-}
+
+
+
+ /*   public void onButtonSubmit(View v) {
+            Button submit = (Button) findViewById(R.id.submit);
+            submit.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    startActivity(new Intent(TenantLease.this, TenantLeaseConfirm.class));
+                }
+            });
+*/
+        //still need method for sending submission to database once
+        //database connection is working
+
+        public void onRadioButtonClicked(View view) {
+            // Is the button now checked?
+            boolean checked = ((RadioButton) view).isChecked();
+
+            // Check which radio button was clicked
+            switch(view.getId()) {
+                case R.id.yes:
+                    if (checked)
+                        // send info to database
+                        break;
+                case R.id.no:
+                    if (checked)
+                        // send info to database
+                        break;
+            }
+        }
+
+
+
+    }
+
+//}
