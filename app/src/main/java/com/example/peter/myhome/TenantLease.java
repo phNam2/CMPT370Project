d6f240b119757ecb118bd6e9f42ee98393e48194
Graeme <graemegibbings@gmail.com>
2017-11-21 11:06:00 -0600

    better function name for editText check method

--- app/src/main/java/com/example/peter/myhome/TenantLease.java
+++ app/src/main/java/com/example/peter/myhome/TenantLease.java
@@ -160,7 +160,7 @@ public class TenantLease extends AppCompatActivity {
      *
      */
     private void setButtonToClickable(){
-            if(validate(new EditText[]{nameField, addressField, propertyNameField})){
+            if(makeSureAllEditTextFieldsHaveValuesEntered(new EditText[]{nameField, addressField, propertyNameField})){
                 submit.setClickable(true);
             }
         }
@@ -171,7 +171,7 @@ public class TenantLease extends AppCompatActivity {
      * @return
      * True if all fields are non-empty
      */
-    private boolean validate(EditText[] fields){
+    private boolean makeSureAllEditTextFieldsHaveValuesEntered(EditText[] fields){
         for(int i = 0; i < fields.length; i++){
             EditText currentField = fields[i];
             if(currentField.getText().toString().length() <= 0){
