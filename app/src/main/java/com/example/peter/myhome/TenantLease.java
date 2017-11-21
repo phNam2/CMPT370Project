4f66b6ad8554b2406a4c4fe3529b03b283665f08
Graeme <graemegibbings@gmail.com>
2017-11-21 08:54:02 -0600

    Two new methods, validate checks if editTexts are empty, and another to set the submit button to clickable.

--- app/src/main/java/com/example/peter/myhome/TenantLease.java
+++ app/src/main/java/com/example/peter/myhome/TenantLease.java
@@ -6,16 +6,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import static com.example.peter.myhome.R.id.address;
+import static com.example.peter.myhome.R.id.name;
+import static com.example.peter.myhome.R.id.propertyName;
+
 public class TenantLease extends AppCompatActivity {
 
-
-
+    public EditText nameField;
+    public EditText addressField;
+    public EditText propertyNameField;
+    public Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,20 +43,21 @@ public class TenantLease extends AppCompatActivity {
 
 
         //Text fields for tenant renewal form
-        EditText name = (EditText) findViewById(R.id.name);
-        EditText address = (EditText) findViewById(R.id.address);
-        EditText propertyName = (EditText) findViewById(R.id.propertyName);
+        nameField = (EditText) findViewById(name);
+        addressField = (EditText) findViewById(R.id.address);
+        propertyNameField = (EditText) findViewById(R.id.propertyName);
 
         //Radio Buttons for lease renewal
         RadioButton yes = (RadioButton) findViewById(R.id.yes);
         RadioButton no = (RadioButton) findViewById(R.id.no);
 
+
         //create button object for xml submit button
-        Button submit = (Button) findViewById(R.id.submit);
+        submit = (Button) findViewById(R.id.submit);
         //set button to be unclickable after it's created. Will change after fields are filled out
         submit.setClickable(false);
         //when clicked the button sends a toast
-        
+
         submit.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -59,6 +68,58 @@ public class TenantLease extends AppCompatActivity {
 
             }
         });
+
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setButtonToClickable();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setButtonToClickable();
+            }
+        });
+
+        addressField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setButtonToClickable();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setButtonToClickable();
+            }
+        });
+
+        propertyNameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setButtonToClickable();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setButtonToClickable();
+            }
+        });
+
 
     }
 
@@ -94,7 +155,31 @@ public class TenantLease extends AppCompatActivity {
             }
         }
 
+    /**
+     * Checks all editText fields and if they are not empty it will set button to be clickable
+     *
+     */
+    public void setButtonToClickable(){
+            if(true == validate(new EditText[]{nameField, addressField, propertyNameField})){
+                submit.setClickable(true);
+            }
+        }
 
+    /**
+     * checks if all editTexts are empty
+     * @param fields
+     * @return
+     * True if all fields are non-empty
+     */
+    private boolean validate(EditText[] fields){
+        for(int i = 0; i < fields.length; i++){
+            EditText currentField = fields[i];
+            if(currentField.getText().toString().length() <= 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     }
 
