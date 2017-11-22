c174116045057756820cccf707dc05d1b0164f38
Nam Pham <hnp435@mail.usask.ca>
2017-11-21 19:51:05 -0600

    fix files

--- app/src/main/java/com/example/peter/myhome/LandlordLease.java
+++ app/src/main/java/com/example/peter/myhome/LandlordLease.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class LandlordLease extends AppCompatActivity {
 
