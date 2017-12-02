be5522d9c38a92beb8b402ccb1f2d25f0f96f364
Nam Pham <hnp435@mail.usask.ca>
2017-12-02 17:31:32 -0600

    finish payment side

--- app/src/main/java/com/example/peter/myhome/Payment/PaymentHistoryActivity.java
+++ app/src/main/java/com/example/peter/myhome/Payment/PaymentHistoryActivity.java
@@ -1,23 +1,112 @@
 package com.example.peter.myhome.Payment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.peter.myhome.Current_User;
 import com.example.peter.myhome.R;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * Created by Nam on 2017-12-01.
  */
 
-public class PaymentHistoryActivity extends AppCompatActivity {
+public class PaymentHistoryActivity extends AppCompatActivity implements Runnable{
 
+    private String errmsg="";
+    private LinkedList<Integer> amount = null;
+    private LinkedList<String> met = null;
+    private LinkedList<Timestamp> date = null;
+
+    public void run() {
+
+        amount = new LinkedList<Integer>();
+        met = new LinkedList<String>();
+        date = new LinkedList<Timestamp>();
+        try{
+
+            Connection con = PaymentActivity.con;
+            try{
+                String sql;
+
+                sql
+                        = "SELECT Amount, Method, Payment_date FROM Payment WHERE Tenant_ID=?";
+                PreparedStatement prest = con.prepareStatement(sql);
+
+                prest.setInt(1, Current_User.getUserID());
+
+                ResultSet rs = prest.executeQuery();
+                while (rs.next()){
+
+                    amount.add(rs.getInt("Amount"));
+                    met.add(rs.getString( "Method"));
+                    date.add(rs.getTimestamp("Payment_date"));
+                }
+
+                prest.close();
+            }
+            catch (SQLException s){
+                System.out.println("SQL statement is not executed!");
+                errmsg=errmsg+s.getMessage();
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            errmsg=errmsg+e.getMessage();
+        }
+
+        handler.sendEmptyMessage(0);
+
+    }
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+
+            String s1 = "";
+            String s2 = "";
+            String s3 = "";
+            Iterator Iterator1 = amount.iterator();
+            Iterator Iterator2 = met.iterator();
+            Iterator Iterator3 = date.iterator();
+
+            //while (Iterator1.hasNext() && Iterator2.hasNext()) {
+
+            //    s = s + Iterator1.next() + "\t\t" + Iterator2.next() + "\t\t" +"\n";
+            //}
+            while (Iterator1.hasNext() && Iterator2.hasNext() && Iterator3.hasNext()) {
+
+                s1 = s1 + Iterator1.next() + "\n";
+                s2 = s2 + Iterator2.next() + "\n";
+                s3 = s3 + Iterator3.next() + "\n";
+
+            }
+
+            TextView textView1 = (TextView) findViewById(R.id.amount_view);
+            textView1.setText(s1);
+            TextView textView2 = (TextView) findViewById(R.id.method_view);
+            textView2.setText(s2);
+            TextView textView3 = (TextView) findViewById(R.id.date_view);
+            textView3.setText(s3);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_history);
+
+        Thread thread = new Thread(this);
+        thread.start();
     }
-
-
 
 }
